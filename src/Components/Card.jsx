d52eba795e5de5f378cbf3f3ad57fa736198c6ad
Mohaiminul007 <mohaimin.durjoy@gmail.com>
2024-03-19 23:33:48 +0600

    upload page added

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -12,10 +12,10 @@ function Card() {
     const jpLettersAllData = await jpData.jpLetterHiraganas;
     setJpLettersAll(jpLettersAllData)
   }
-  getjpDatafunc()
   
-  // useEffect(() => {
-  // }, [jpLettersAll])
+  useEffect(() => {
+    getjpDatafunc()
+  }, [])
   
 
   // console.log('=>>>>>>>>>>>>>',jpLettersAll);
@@ -27,25 +27,26 @@ function Card() {
 
 
   return (
-    <div className='text-white bg-gray-900'>
-        <div>
-        <h2 className="text-5xl mb-3 text-center">Hiragana</h2>
-        <hr className="border-2 border-blue-400"/>
+    <div className=''>
+        <h2 className="text-5xl  mb-3 text-center">Hiragana</h2>
+        <hr className="border-2 border-gray-400"/>
+        <div className='mt-5'>
         {
           jpLettersAll && jpLettersAll.map((item,index) => (
-            <div>
+            <div className='mt-5'>
             <div className=" flex flex-col items-center 
             md:grid md:grid-cols-3 gap-y-4 
-            border-2 border-blue-400 
-            m-2 p-2 rounded-md pl-5 ">
+            border-2 border-blue-400
+            shadow-xl 
+            m-2 p-4 rounded-md pl-5 ">
                 <div className='flex items-center justify-center'>
-                <img className="invert rounded-3xl 
+                <img className=" rounded-3xl 
                 ring-4 
                 ring-cyan-400" 
                 src={item.imageHiragana.url}
                 alt="img-name" 
                 key={item.imageHiragana.id}
-                width="50%"/>
+                width="60%"/>
                 </div>
              
               
@@ -54,11 +55,14 @@ function Card() {
               </div>
           
                 
-                  <div className="text-center flex flex-col gap-y-4 items-center justify-center">
+                  <div className="text-center 
+                  border-2 border-cyan-400
+                  flex flex-col gap-y-4 items-center justify-center">
                     <h3 className="text-5xl font-semibold">
                         <span className="text-[10rem]">{item.jpSingleLetterHiragana}</span>{item.englishPronounciationHiragana}
-                    </h3>
-                    <audio controls controlsList="nodownload noplaybackrate" >
+                    </h3>J
+                    <audio controls className='border-2 border-cyan-200 rounded-full' 
+                    controlsList="nodownload noplaybackrate" >
                     <source src={item.audioHiragana.url} 
                     key={item.audioHiragana.id}
                     />
--- src/app/upload/page.jsx
+++ src/app/upload/page.jsx
@@ -1,23 +1,93 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
 const Upload = () => {
-  return (
-    <div>
-        <h1>writing Gif:</h1>
-        <input type="file" />
-        <br />
-        <br />
-        <h1>Japanes Letter:</h1>
-        <input className='border-2 border-cyan-400 rounded-md' type="text" />
-        <br />
-        <br />
-        <h1>English pronunciation:</h1>
-        <input className='border-2 border-cyan-400 rounded-md' type="text" />
-        <br />
-        <br />
-        <h1>Japanes Audio:</h1>
-        <input type="file" />
 
+  const [formValue,setFormValue] = useState({
+    letterCatagory:"",
+    drawingImage:"",
+    japanesLetter:"",
+    serialNumber:"",
+    englishPronunciation:"",
+    japanesAudio:""
+  })
+
+
+
+  
+  const handleChange = (event)=>{
+   setFormValue({
+    ...formValue,[event.target.name]:event.target.value
+    
+   }) 
+   console.log("=>>>>>>>>>", event.target.value)
+  }
+
+  const handleSubmit = () => {
+    console.log(formValue)
+  }
+  
+  
+  return (
+    <div >
+        <div className='flex flex-col items-center justify-center
+      
+      '>
+        <h1 className='font-bold text-6xl my-2'>Input Here</h1>
+        <form action="" className='border-2 border-cyan-300 
+        flex  items-center justify-center mt-5
+        rounded-xl p-5' >
+
+        <div className=''>
+          <h1 className='font-semibold'>Letter Catagory</h1>
+          
+                {/* <label for="cars">Choose a car:</label> */}
+        <select className='border-2  border-cyan-400 rounded-md mb-5' name="letterCatagory"
+          onChange={handleChange}
+        >
+          <option  defaultChecked value='' >Select One</option>
+          <option>Hiragana</option>
+          <option>Katakana</option>
+          <option>Kanji</option>
+          
+        </select>
+
+
+          <h1 className='font-semibold'>Drawing Image:</h1>
+          <input type="file" name='drawingImage'  onChange={handleChange} className='mb-5' />
+          
+          <h1 className='font-semibold'>Japanes Letter:</h1>
+          <input className='border-2 border-cyan-400 rounded-md mb-5'  onChange={handleChange} name='japanesLetter' type="text" />
+          
+
+          <h1 className='font-semibold'>Serial Number:</h1>
+          <input className='border-2 border-cyan-400 rounded-md mb-5'  onChange={handleChange} name='serialNumber' type="text" />
+          
+
+
+          <h1 className='font-semibold'>English Pronunciation:</h1>
+          <input className='border-2 border-cyan-400 rounded-md mb-5'  onChange={handleChange} name='englishPronunciation' type="text" />
+          
+          <h1 className='font-semibold'>Japanes Audio:</h1>
+          <input type="file" name='japanesAudio'  onChange={handleChange} className='mb-5'/>
+
+
+          <div className='flex justify-center'>
+            <button className='border border-blue-500 p-1 m-1 text-white  bg-blue-700
+            hover:bg-blue-500 duration-300 px-4 py-2 text-lg
+            rounded-lg'
+            type="button"
+
+            onClick={handleSubmit}
+
+            >Submit</button>
+          </div>
+
+          {/* ✅ */}
+
+        </div>
+      </form>
+        </div>
     </div>
   )
 }
