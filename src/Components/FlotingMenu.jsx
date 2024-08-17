f2482bbb5ec5dbf1127de83630d631dc18958b6b
Mohaiminul007 <mohaimin.durjoy@gmail.com>
2024-08-18 05:42:51 +0600

    at the rate issue solved

--- src/Components/FlotingMenu.jsx
+++ src/Components/FlotingMenu.jsx
@@ -6,7 +6,7 @@ import {
   useScroll,
   useMotionValueEvent,
 } from "framer-motion";
-import { cn } from "@/utils/cn";
+import { cn } from "../utils/cn";
 import Link from "next/link";
 import MagicBtn from "./ui/MagicBtn";
 
