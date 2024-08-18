bde608f016b2a7b7ec99605807412486880100fe
Mohaiminul007 <mohaimin.durjoy@gmail.com>
2024-08-18 20:01:13 +0600

    next config edited

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   
+    reactStrictMode: true,
+    experimental: {
+    outputFileTracingIgnores: ['canvas'],
+    },
 };
 
 export default nextConfig;
